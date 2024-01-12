821a0df962ccd821785029135c9b7b2dad23acda
ayyn0863 <140891515+ayyn0863@users.noreply.github.com>
2024-01-12 11:42:09 +0900

    編集

--- src/reg.php
+++ src/reg.php
@@ -2,10 +2,10 @@
 <html lang="ja">
 	<head>
 		<meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
 		<link rel="stylesheet" href="../css/style.css">
-		<!-- <link rel="stylesheet" href="../css/reg.css"> -->
-		<!-- <link rel="stylesheet" href="../css/iroiro.css"> -->
+		<link rel="stylesheet" href="../css/reg.css">
+		<link rel="stylesheet" href="../css/iroiro.css">
         <title>猫の里親-登録画面-</title>
 	</head>
 	<body>
@@ -14,8 +14,8 @@
 
         <h1 class="sample">登録</h1>
 
-        <!-- <div class="container">
-            <div class="left-aligned-text"> -->
+        <div class="container">
+            <div class="left-aligned-text">
 
                 猫ちゃんのお名前<br>
                 <input v-model="name" type="text" class="text" name="catname"><br>
@@ -40,9 +40,9 @@
                 <div class="bobo">
                     <button class="example" type="button" @click="submitForm"><span>登録する</span></button>
                 </div>
-            <!-- </div>
-        </div> -->
-            <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
-            <script src="../script/reg.js"></script>
+            </div>
+        </div>
+            <!-- <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+            <script src="../script/reg.js"></script> -->
     </body>
 </html>
