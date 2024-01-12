f14d9c8fa303627314bc53017046e52edd4daa42
ayyn0863 <140891515+ayyn0863@users.noreply.github.com>
2024-01-12 12:06:40 +0900

    編集

--- src/reg.php
+++ src/reg.php
@@ -2,7 +2,7 @@
 <html lang="ja">
 	<head>
 		<meta charset="UTF-8">
-        <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="css/style.css">
 		<link rel="stylesheet" href="css/reg.css">
 		<link rel="stylesheet" href="css/iroiro.css">
@@ -71,7 +71,7 @@
             </div>
         </div>
         </form>
-            <!-- <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
-            <script src="script/reg.js"></script> -->
+            <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+            <script src="script/reg.js"></script>
     </body>
 </html>
