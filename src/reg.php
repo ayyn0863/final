e1eb3f4ead897baaca28e5f4357c1d79fe3b69af
ayyn0863 <140891515+ayyn0863@users.noreply.github.com>
2024-01-12 11:47:02 +0900

    編集

--- src/reg.php
+++ src/reg.php
@@ -3,9 +3,9 @@
 	<head>
 		<meta charset="UTF-8">
         <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
-		<link rel="stylesheet" href="../css/style.css">
-		<link rel="stylesheet" href="../css/reg.css">
-		<link rel="stylesheet" href="../css/iroiro.css">
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="css/reg.css">
+		<link rel="stylesheet" href="css/iroiro.css">
         <title>猫の里親-登録画面-</title>
 	</head>
 	<body>
@@ -42,7 +42,8 @@
                 </div>
             </div>
         </div>
+        </form>
             <!-- <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
-            <script src="../script/reg.js"></script> -->
+            <script src="script/reg.js"></script> -->
     </body>
 </html>
