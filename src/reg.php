00626240e731d8c5473e3f72082f1f11e65e6fd4
ayyn0863 <140891515+ayyn0863@users.noreply.github.com>
2024-01-15 12:44:06 +0900

    編集

--- src/reg.php
+++ src/reg.php
@@ -48,6 +48,7 @@
             </div>
         </div>
         </form>
+            <script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
             <script src="script/reg.js"></script>
     </body>
