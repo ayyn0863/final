31697681e32b48210619e7d3ddc12a5d2335288b
ayyn0863 <140891515+ayyn0863@users.noreply.github.com>
2024-01-15 22:54:09 +0900

    編集

---
+++ src/update-output.php
@@ -0,0 +1,35 @@
+<?php session_start()?>
+<?php require 'connect.php' ;?>
+<?php 
+    $pdo = new PDO($connect,USER,PASS);
+    if(isset($_SESSION['customer'])){
+        $id=$_SESSION['customer']['id'];
+        $sql=$pdo->prepare('select * from customer where id != ? and login = ?');
+        $sql->execute([$id, $_POST['login']]);
+    }else{
+        $sql=$pdo->prepare('select * from customer where login = ?');
+        $sql->execute([$_POST['login']]);
+    }
+    if(empty($sql->fetchAll())){
+        if (isset($_SESSION['customer'])) {
+            $sql=$pdo->prepare('update customer set name = ?, address= ?, login = ?, password = ? where id = ?');
+            $sql->execute([
+                $_POST['name'],$_POST['address'],$_POST['login'],password_hash($row['password'],PASSWORD_DEFAULT),$id
+            ]);
+            $_SESSION['customer'] = [
+                'id' => $id, 'name' => $_POST['name'],
+                'address' => $_POST['address'], 'login' => $_POST['login'],
+                'password' => password_hash($_POST['password'],PASSWORD_DEFAULT)];
+            echo 'お客様情報を更新しました。';
+        }else{
+            $sql=$pdo->prepare('insert into customer values(null,?,?,?,?)');
+            $sql->execute([
+                $_POST['name'],$_POST['address'],$_POST['login'],password_hash($_POST['password'],PASSWORD_DEFAULT)
+            ]);
+            echo 'お客様情報を登録しました。';
+        }
+    }else{
+        echo 'ログイン名が既に使用されていますので、変更してください。';
+    }    
+?>
+<?php require 'footer.php'; ?>
--- src/update.php
+++ src/update.php
@@ -1,3 +1,6 @@
+<?php session_start(); ?>
+<?php require 'header.php'; ?>
+
 <!DOCTYPE html>
 <html lang="ja">
 	<head>
@@ -9,5 +12,23 @@
         <div class="bg_pattern Paper_v2"></div>
 		<a href="index.html" class="btn btn-border"><span>戻る</span></a>
         <h1 class="sample">更新</h1>
-    </body>
+			<?php
+			$name=$hinsyu=$text='';
+			if(isset($_SESSION['Cat'])){
+				$name=$_SESSION['Cat']['catname'];
+				$text=$_SESSION['Cat']['text'];
+			}
+			echo '<form action="update-output.php" method="post">';
+			echo '<table>';
+			echo '<tr><td>猫のお名前</td><td>';
+			echo '<input type="text" name="name" value="', $name,'">';
+			echo '</td></tr>';
+			echo '<tr><td>説明</td><td>';
+			echo '<input type="text" name="text" value="', $text,'">';
+			echo '</td></tr>';
+			echo '</table>';
+			echo '<input type="submit" value="確定">';
+			echo '</form>';
+			?>
+	</dody>
 </html>
