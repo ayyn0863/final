a8e58f77cf7ea0921555ddaab733ff51830c7868
ayyn0863 <140891515+ayyn0863@users.noreply.github.com>
2024-01-15 21:34:04 +0900

    編集

--- src/reg.php
+++ src/reg.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/reg.css">
+    <!-- <link rel="stylesheet" href="css/reg.css"> -->
     <link rel="stylesheet" href="css/iroiro.css">
     <title>猫の里親-登録画面-</title>
 </head>
