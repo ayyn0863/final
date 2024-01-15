3f7dadba208b9fb6f59493b67a83fd3e08b15458
ayyn0863 <140891515+ayyn0863@users.noreply.github.com>
2024-01-15 21:28:51 +0900

    編集

--- src/reg.php
+++ src/reg.php
@@ -19,6 +19,19 @@
                 猫ちゃんのお名前<br>
                 <input v-model="name" type="text" class="text" name="catname"><br>
                 <div v-if="NameError" class="error">猫ちゃんのお名前を入力してください</div>
+
+
+                <br>
+                猫ちゃんの品種wk<br>
+                <div>
+                    <select name="catbreedid2" id="select-breed2">
+                        <option value="1">アメリカンショートヘア</option>
+                        <option value="2">シャム</option>
+                    </select>
+                </div>
+
+
+
                 <br>
                 猫ちゃんの品種<br>
                 <div class="sel sel--black-panther">
