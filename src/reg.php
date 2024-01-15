09a91e1cd9df4e6798a951fac4d74d5bb1992225
ayyn0863 <140891515+ayyn0863@users.noreply.github.com>
2024-01-15 20:02:39 +0900

    編集

--- src/reg.php
+++ src/reg.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="ja">
-	<head>
-		<meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="stylesheet" href="css/style.css">
-		<link rel="stylesheet" href="css/reg.css">
-		<link rel="stylesheet" href="css/iroiro.css">
-        <title>猫の里親-登録画面-</title>
-	</head>
-	<body>
-        <form id="appForm" action="reg-output.php" method="post">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/reg.css">
+    <link rel="stylesheet" href="css/iroiro.css">
+    <title>猫の里親-登録画面-</title>
+</head>
+<body>
+    <form id="appForm" action="reg-output.php" method="post">
         <div class="bg_pattern Paper_v2"></div>
         <a href="index.html" class="btn btn-border"><span>戻る</span></a>
         <h1 class="sample">登録</h1>
@@ -19,37 +19,37 @@
 
                 猫ちゃんのお名前<br>
                 <input v-model="name" type="text" class="text" name="catname"><br>
-                <div v-if="NameError" class="error">お名前を入力してください</div>
+                <div v-if="NameError" class="error">猫ちゃんのお名前を入力してください</div>
                 <br>
                 猫ちゃんの品種<br>
-                    <div class="sel sel--black-panther">
-                        <select name="catbreedid" id="select-breed">
-                            <option value="" disabled>品種</option>
-                            <option value="1">アメリカンショートヘア</option>
-                            <option value="2">シャム</option>
-                            <option value="3">スコティッシュフォールド</option>
-                            <option value="4">ノルウェージャンフォレストキャット</option>
-                            <option value="5">ペルシャ</option>
-                            <option value="6">マンチカン</option>
-                            <option value="7">メインクーン</option>
-                            <option value="8">ラグドール</option>
-                            <option value="9">ロシアンブルー</option>
-                            <option value="10" disabled>雑種</option>  
-                        </select>
-                    </div>
+                <div class="sel sel--black-panther">
+                    <select v-model="hinsyu" name="catbreedid" id="select-breed">
+                        <option value="" disabled>品種</option>
+                        <option value="1">アメリカンショートヘア</option>
+                        <option value="2">シャム</option>
+                        <option value="3">スコティッシュフォールド</option>
+                        <option value="4">ノルウェージャンフォレストキャット</option>
+                        <option value="5">ペルシャ</option>
+                        <option value="6">マンチカン</option>
+                        <option value="7">メインクーン</option>
+                        <option value="8">ラグドール</option>
+                        <option value="9">ロシアンブルー</option>
+                        <option value="10">雑種</option>  
+                    </select>
+                </div>
                 <br>
                 説明<br>
                 <textarea v-model="text" class="text" name="cattext" cols="30" rows="5" placeholder="説明を入力してください"></textarea><br>
                 <div v-if="TextError" class="error">説明を入力してください</div>
                 <br>
                 <div class="bobo">
-                    <a href="reg-output.php" class="btn btn-border" @click="submitForm"><span>登録する</span></a>
+                    <button class="btn btn-border" @click.prevent="submitForm"><span>登録する</span></button>
                 </div>
             </div>
         </div>
-        </form>
-            <script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-            <script src="script/reg.js"></script>
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="script/reg.js"></script>
     </body>
 </html>
--- src/script/reg.js
+++ src/script/reg.js
@@ -1,91 +1,87 @@
 /* ===== Logic for creating fake Select Boxes ===== */
 $('.sel').each(function() {
-  $(this).children('select').css('display', 'none');
-  
-  var $current = $(this);
-  
-  $(this).find('option').each(function(i) {
+$(this).children('select').css('display', 'none');
+  var $current = $(this);   
+$(this).find('option').each(function(i) {
     if (i == 0) {
-      $current.prepend($('<div>', {
+        $current.prepend($('<div>', {
         class: $current.attr('class').replace(/sel/g, 'sel__box')
-      }));
-      
-      var placeholder = $(this).text();
+    }));
+                  
+    var placeholder = $(this).text();
       $current.prepend($('<span>', {
         class: $current.attr('class').replace(/sel/g, 'sel__placeholder'),
         text: placeholder,
-        'data-placeholder': placeholder
+    'data-placeholder': placeholder
       }));
-      
+                  
       return;
-    }
-    
-    $current.children('div').append($('<span>', {
-      class: $current.attr('class').replace(/sel/g, 'sel__box__options'),
-      text: $(this).text()
-    }));
-  });
-});
+      }
+              
+              $current.children('div').append($('<span>', {
+                  class: $current.attr('class').replace(/sel/g, 'sel__box__options'),
+                  text: $(this).text()
+              }));
+          });
+      });
 
-// Toggling the `.active` state on the `.sel`.
-$('.sel').click(function() {
-  $(this).toggleClass('active');
-});
+      // Toggling the `.active` state on the `.sel`.
+      $('.sel').click(function() {
+          $(this).toggleClass('active');
+      });
 
-// Toggling the `.selected` state on the options.
-$('.sel__box__options').click(function() {
-  var txt = $(this).text();
-  var index = $(this).index();
-  
-  $(this).siblings('.sel__box__options').removeClass('selected');
-  $(this).addClass('selected');
-  
-  var $currentSel = $(this).closest('.sel');
-  $currentSel.children('.sel__placeholder').text(txt);
-  $currentSel.children('select').prop('selectedIndex', index + 1);
-});
+      // Toggling the `.selected` state on the options.
+      $('.sel__box__options').click(function() {
+          var txt = $(this).text();
+          var index = $(this).index();
+          
+          $(this).siblings('.sel__box__options').removeClass('selected');
+          $(this).addClass('selected');
+          
+          var $currentSel = $(this).closest('.sel');
+          $currentSel.children('.sel__placeholder').text(txt);
+          $currentSel.children('select').prop('selectedIndex', index + 1);
+      });
 
-new Vue({
-  el: '#appForm',
-  data: {
-    name: '',
-    hinsyu: '', // hinsyuのデータプロパティを追加
-    text: '',
-    NameError: false,
-    HinsyuError: false, // HinsyuErrorプロパティの名前を修正
-    TextError: false
-  },
-  methods: {
-    validateForm: function () {
-      this.NameError = this.name.trim() === '';
-      this.HinsyuError = this.hinsyu.trim() === '' || this.hinsyu === null; // hinsyuが選択されているか確認
-      this.TextError = this.text.trim() === '';
-
-      // エラーがある場合はtrueを返す
-      return this.NameError || this.HinsyuError || this.TextError;
-    },
-    submitForm: function () {
-      if (!this.validateForm()) {
-        // バリデーションが通過した場合、フォームを送信
-        document.getElementById('appForm').submit();
+      new Vue({
+          el: '#appForm',
+          data: {
+              name: '',
+              hinsyu: '',
+              text: '',
+              NameError: false,
+              HinsyuError: false,
+              TextError: false
+          },
+          methods: {
+              validateForm: function () {
+                  this.NameError = this.name.trim() === '';
+                  this.HinsyuError = this.hinsyu.trim() === '' || this.hinsyu === null;
+                  this.TextError = this.text.trim() === '';
+                  
+                  return this.NameError || this.HinsyuError || this.TextError;
+              },
+              submitForm: function () {
+                  if (!this.validateForm()) {
+                      document.getElementById('appForm').submit();
+                  }
+              }
+          },
+          watch: {
+              name: function () {
+                  if (this.name.trim() !== '') {
+                      this.NameError = false;
+                  }
+              },
+        hinsyu: function () {
+          if (this.hinsyu !== null && this.hinsyu.trim() !== '') {
+                this.HinsyuError = false;
+              }
+        },
+        text: function () {
+          if (this.text.trim() !== '') {
+                this.TextError = false;
+          }
       }
     }
-  },
-  watch: {
-    name: function () {
-      if (this.name.trim() !== '') {
-        this.NameError = false; // エラーがない場合、エラーメッセージをクリア
-      }
-    },
-    hinsyu: function () {
-      if (this.hinsyu !== null && this.hinsyu.trim() !== '') {
-        this.HinsyuError = false; // エラーがない場合、エラーメッセージをクリア
-      }
-    },
-    text: function () {
-      if (this.text.trim() !== '') {
-        this.TextError = false; // エラーがない場合、エラーメッセージをクリア
-      }
-    }
-  }
 });
