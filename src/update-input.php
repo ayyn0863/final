7b0100931f0cf7d5bf9ed479f89b820a25b369c4
ayyn0863 <140891515+ayyn0863@users.noreply.github.com>
2024-01-16 09:51:01 +0900

    編集

--- src/update-input.php
+++ src/update-input.php
@@ -70,7 +70,7 @@
 
     <script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-    <script src="script/update.js"></script>
+    <script src="script/update2.js"></script>
 
 </body>
 </html>
