815bf7d60155afce7d5dcfcab34e15501ebe3cb9
ayyn0863 <140891515+ayyn0863@users.noreply.github.com>
2024-01-17 09:34:50 +0900

    編集

--- src/delete.php
+++ src/delete.php
@@ -51,7 +51,7 @@
                 <div class="bobo">
                     <a href="index.html" class="btn btn-border"><span>戻る</span></a>
                     <?php if (!empty($cats)): ?>
-						<a type="submit" class="btn btn-border" id="editLink"><span>削除</span></a>
+						<button type="submit" class="btn btn-border" id="editLink"><span>削除</span></button>
                     <?php endif; ?>
                 </div>
             </form>
