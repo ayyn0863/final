3d827e3c2e1f7954b02a89e94b6701678dbbd61e
ayyn0863 <140891515+ayyn0863@users.noreply.github.com>
2024-01-17 09:16:20 +0900

    編集

--- src/delete.php
+++ src/delete.php
@@ -59,6 +59,6 @@
     </div>
     <script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-    <script src="script/update1.js"></script>
+    <script src="script/delete.js"></script>
 </body>
 </html>
--- src/script/delete.js
+++ src/script/delete.js
@@ -1,8 +1,8 @@
 document.getElementById('editLink').addEventListener('click', function() {
     var selectedCatId = document.querySelector('input[name="catid"]:checked');
     if (selectedCatId) {
-        // 選択されたラジオボタンがあれば、update-input.phpにリダイレクト
-        window.location.href = 'update-input.php?catid=' + selectedCatId.value;
+        // 選択されたラジオボタンがあれば、delete-output.phpにリダイレクト
+        window.location.href = 'delete-output.php?catid=' + selectedCatId.value;
     } else {
         alert('猫を選択してください。');
     }
