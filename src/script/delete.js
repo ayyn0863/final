d4780c4e6ea8438ad036eaae74ac52433fe9ce14
ayyn0863 <140891515+ayyn0863@users.noreply.github.com>
2024-01-17 10:38:44 +0900

    編集

--- src/script/delete.js
+++ src/script/delete.js
@@ -2,3 +2,23 @@ function showAlertAndRedirect() {
     alert("削除が成功しました。");
     window.location.href = "list.php";
 }
+
+function validateForm() {
+    // ラジオボタンが選択されているかを確認
+    var radioButtons = document.getElementsByName("catid");
+    var radioButtonChecked = false;
+
+    for (var i = 0; i < radioButtons.length; i++) {
+        if (radioButtons[i].checked) {
+            radioButtonChecked = true;
+            break;
+        }
+    }
+
+    if (!radioButtonChecked) {
+        alert("削除する猫を選択してください。");
+        return false;
+    }
+
+    return true;
+}
