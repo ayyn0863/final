6d1b0d30a85b13b98c3debb604d49abdc53a0979
ayyn0863 <140891515+ayyn0863@users.noreply.github.com>
2024-01-17 09:18:28 +0900

    編集

--- src/delete.php
+++ src/delete.php
@@ -57,8 +57,5 @@
             </form>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-    <script src="script/delete.js"></script>
 </body>
 </html>
--- src/script/delete.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('editLink').addEventListener('click', function() {
-    var selectedCatId = document.querySelector('input[name="catid"]:checked');
-    if (selectedCatId) {
-        // 選択されたラジオボタンがあれば、delete-output.phpにリダイレクト
-        window.location.href = 'delete-output.php?catid=' + selectedCatId.value;
-    } else {
-        alert('猫を選択してください。');
-    }
-});
