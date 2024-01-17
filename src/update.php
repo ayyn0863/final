0d27a5334f7a11cf630fdf08f7e8f9ced2d253a4
ayyn0863 <140891515+ayyn0863@users.noreply.github.com>
2024-01-17 11:06:57 +0900

    編集

--- src/update.php
+++ src/update.php
@@ -26,8 +26,8 @@
             </form>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/vue@2"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-    <script src="script/update1.js"></script>
+    <script src="script/update1.js"></script> -->
 </body>
 </html>
